48e1520f11045950d4f32c1b71da9d5e51b4210c
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-17 17:31:14 +0200

    second snack html add

--- second_snack.php
+++ second_snack.php
@@ -2,3 +2,19 @@
 $name = $_GET['name'] ?? '';
 $email = $_GET['email'] ?? '';
 $age = $_GET['age'] ?? '';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Second snack</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
